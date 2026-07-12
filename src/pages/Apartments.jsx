0d1e01827e9d4cff34a96e61f037f691739a6264
Дмитрий <dimanv2002@bk.ru>
2026-07-12 15:50:01 +0300

    добавление поисковой строки в кв

--- src/pages/Apartments.jsx
+++ src/pages/Apartments.jsx
@@ -1,28 +1,74 @@
+import { useState } from "react"
 import ApartmentCard from "../components/ApartmentCard"
 import apartments from "../data/apartments"
 import "./Apartments.css"
 
 function Apartments() {
+
+  const [search, setSearch] = useState("")
+
+  const filteredApartments = apartments.filter((item) =>
+    item.address
+      .toLowerCase()
+      .includes(search.toLowerCase())
+  )
+
   return (
+
     <main className="apartments">
 
       <h1>
         Наши квартиры
       </h1>
 
+      <div className="search-mobile">
+
+        <input
+
+          type="text"
+
+          placeholder="Поиск по адресу..."
+
+          value={search}
+
+          onChange={(e) => setSearch(e.target.value)}
+
+        />
+
+      </div>
+
       <div className="apartments-list">
 
-        {apartments.map((item) => (
-          <ApartmentCard
-            key={item.id}
-            apartment={item}
-          />
-        ))}
+        {filteredApartments.length > 0 ? (
+
+          filteredApartments.map((item) => (
+
+            <ApartmentCard
+
+              key={item.id}
+
+              apartment={item}
+
+            />
+
+          ))
+
+        ) : (
+
+          <p className="no-result">
+
+            Квартира не найдена
+
+          </p>
+
+        )}
 
       </div>
 
     </main>
+
   )
+
 }
 
 export default Apartments
